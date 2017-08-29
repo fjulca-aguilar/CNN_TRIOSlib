9608cc9fe6a1401d43237742931553fa6d3de4c9
fjulca-aguilar <frank.julca@gmail.com>
2017-08-29 11:21:45 -0300

    Add scopes to CNN_TFClassifier.py

--- CNN_TFClassifier.py
+++ CNN_TFClassifier.py
@@ -31,47 +31,68 @@ class CNN_TFClassifier():
 	def prepare_graph(self):
 		imageSize = self.input_shape[0] * self.input_shape[1]
 
-		x_ = tf.placeholder(tf.float32, shape=[None, self.input_shape[0], self.input_shape[1], self.input_shape[2]])
-		y_ = tf.placeholder(tf.float32, shape=[None, self.num_outputs])
+		with tf.name_scope("data"):
+			x_ = tf.placeholder(tf.float32, shape=[None, self.input_shape[0], 
+				self.input_shape[1], self.input_shape[2]], name="input")
+			y_ = tf.placeholder(tf.float32, shape=[None, self.num_outputs], name="labels")
 
-		W_conv1 = self.weight_variable(self.first_conv)
-		b_conv1 = self.bias_variable([self.first_conv[3]])
+		with tf.name_scope("conv1"):
+			W_conv1 = self.weight_variable(self.first_conv, name="weight1")
+			b_conv1 = self.bias_variable([self.first_conv[3]], name="bias1")
+			h_conv1 = tf.nn.relu(self.conv2d(x_, W_conv1) + b_conv1)
+		
+		with tf.name_scope("pool1"):
+			h_pool1 = self.max_pool_2x2(h_conv1)
 
-		h_conv1 = tf.nn.relu(self.conv2d(x_, W_conv1) + b_conv1)
-		h_pool1 = self.max_pool_2x2(h_conv1)
-
-		W_conv2 = self.weight_variable(self.second_conv)
-		b_conv2 = self.bias_variable([self.second_conv[3]])
-
-		h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
-		h_pool2 = self.max_pool_2x2(h_conv2)
+		with tf.name_scope("conv2"):
+			W_conv2 = self.weight_variable(self.second_conv, name="weight2")
+			b_conv2 = self.bias_variable([self.second_conv[3]], name="bias2")
+			h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
+			
+		with tf.name_scope("pool2"):
+			h_pool2 = self.max_pool_2x2(h_conv2)
 		
 		new_width = int(math.ceil(self.input_shape[1] / (4. * self.conv_strides[2] * self.conv_strides[2])))
 		new_height = int(math.ceil(self.input_shape[0] / (4. * self.conv_strides[1] * self.conv_strides[1]) ))
+		num_fully_connected = 1024
 
-		W_fc1 = self.weight_variable([new_width * new_height * self.second_conv[3], 1024])
-		b_fc1 = self.bias_variable([1024])
+		with tf.name_scope("fc1"):
+			W_fc1 = self.weight_variable([new_width * new_height * self.second_conv[3], num_fully_connected], name="weight3")
+			b_fc1 = self.bias_variable([num_fully_connected], name="bias3")
+			h_pool2_flat = tf.reshape(h_pool2, [-1, new_width * new_height * 64])
+			h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-		h_pool2_flat = tf.reshape(h_pool2, [-1, new_width * new_height * 64])
-		h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+		with tf.name_scope("dropout1"):
+			keep_prob = tf.placeholder(tf.float32)
+			h_fc1_drop = tf.nn.dropout(h_fc1 , keep_prob)
 
-		keep_prob = tf.placeholder(tf.float32)
-		h_fc1_drop = tf.nn.dropout(h_fc1 , keep_prob)
+		with tf.name_scope("fc2"):
+			W_fc2 = self.weight_variable([num_fully_connected, self.num_outputs], name="weight4")
+			b_fc2 = self.bias_variable([self.num_outputs], name="bias4")
+			y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
-		W_fc2 = self.weight_variable([1024, self.num_outputs])
-		b_fc2 = self.bias_variable([self.num_outputs])
+		with tf.name_scope("loss"):
+			if self.output_activation == 'sigmoid':
+				cross_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y_, logits=y_conv))
+				y_conv_prob = tf.sigmoid(y_conv)
+			else:
+				cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+				y_conv_prob = tf.nn.softmax(y_conv)
 
-		y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-		
-		if self.output_activation == 'sigmoid':
-			cross_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y_, logits=y_conv))
-			y_conv_prob = tf.sigmoid(y_conv)
-		else:
-			cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-			y_conv_prob = tf.nn.softmax(y_conv)
-		wrong_prediction = tf.not_equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-		MAE = tf.reduce_mean(tf.cast(wrong_prediction, tf.float32))
-		return (x_, y_, keep_prob, cross_entropy, y_conv, y_conv_prob)
+		with tf.name_scope("optimizer"):
+			global_step = tf.Variable(0, name='global_step', trainable=False)
+			train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(cross_entropy, global_step=global_step)
+
+		net = {
+			'input': x_,
+			'labels': y_,
+			'keep_prob': keep_prob,
+			'loss': cross_entropy,
+			'probabilities': y_conv_prob,
+			'train_step': train_step,
+			'global_step': global_step
+		}
+		return net
 
 
 	def fit(self, x, y, x_val, y_val):
@@ -79,13 +100,18 @@ class CNN_TFClassifier():
 			os.makedirs(self.model_dir)
 		g = tf.Graph()
 		with g.as_default():
-			x_, y_, keep_prob, cross_entropy, y_conv, y_conv_prob = self.prepare_graph()
+			net = self.prepare_graph()
+			x_ = net['input']
+			y_ = net['labels']
+			keep_prob = net['keep_prob']
+			cross_entropy = net['loss']
+			y_conv_prob = net['probabilities']
+			global_step = net['global_step']
+			train_step = net['train_step']
+
 			session = tf.InteractiveSession()
 			indices = list(range(x.shape[0]))
 			num_mini_batchs = math.floor(x.shape[0] / self.batch_size)
-			global_step = tf.Variable(0, name='global_step', trainable=False)
-			train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(cross_entropy, global_step=global_step)
-						
 			max_accuracy = -1.
 			session.run(tf.initialize_all_variables())
 
@@ -141,11 +167,14 @@ class CNN_TFClassifier():
 			return None
 		g = tf.Graph()
 		with g.as_default():
-			x_, y_, keep_prob, cross_entropy, y_conv, y_conv_prob = self.prepare_graph()
+			net = self.prepare_graph()
+			x_ = net['input']
+			keep_prob = net['keep_prob']
+			y_conv_prob = net['probabilities']
 			session = tf.InteractiveSession()
 			saver = tf.train.Saver()
 			saver.restore(session, tf.train.latest_checkpoint(self.graph_path))
-			sliceSize = 50
+			sliceSize = 1000
 			numSlices = math.ceil(float(x.shape[0]) / sliceSize)
 			outputs = np.zeros((x.shape[0], self.num_outputs), dtype=np.float64)
 			for i in range(numSlices):
@@ -172,13 +201,13 @@ class CNN_TFClassifier():
 			correct_prediction = np.argmax(outputs, 1) == np.argmax(y, 1)
 			return np.mean(correct_prediction, dtype=np.float64)
 
-	def weight_variable(self, shape):
+	def weight_variable(self, shape, name):
 		initial = tf.truncated_normal(shape, stddev = 0.1)
-		return tf.Variable(initial)
+		return tf.Variable(initial, name=name)
 
-	def bias_variable(self, shape):
+	def bias_variable(self, shape, name):
 		initial = tf.constant(0.1, shape = shape)
-		return tf.Variable(initial)
+		return tf.Variable(initial, name=name)
 
 	def conv2d(self, x, W):
 		return tf.nn.conv2d(x, W, strides = self.conv_strides, 
